ea5d9055215734d3fb99cc1037a48c529483b4c6
Marco Fiorito <marcofiorito1@gmail.com>
2023-06-03 00:55:45 -0300

    route actions

--- src/routes/demo/joke/index.tsx
+++ src/routes/demo/joke/index.tsx
@@ -1,5 +1,16 @@
-import { component$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
+import {
+  component$,
+  useSignal,
+  useStylesScoped$,
+  useTask$,
+} from "@builder.io/qwik";
+import {
+  Form,
+  routeAction$,
+  routeLoader$,
+  server$,
+} from "@builder.io/qwik-city";
+import styles from "./index.css?inline";
 
 export const useDadJoke = routeLoader$(async () => {
   const response = await fetch("https://icanhazdadjoke.com/", {
@@ -12,12 +23,44 @@ export const useDadJoke = routeLoader$(async () => {
   };
 });
 
+export const useJokeVoteAction = routeAction$((props) => {
+  // Leave it as an exercise for the reader to implement this.
+  console.log("VOTE", props);
+});
+
 export default component$(() => {
   // Calling our `useDadJoke` hook, will return a reactive signal to the loaded data.
   const dadJokeSignal = useDadJoke();
+  const favoriteJokeAction = useJokeVoteAction();
+  const isFavoriteSignal = useSignal(false);
+  useTask$(({ track }) => {
+    track(() => isFavoriteSignal.value);
+    console.log("FAVORITE (isomorphic)", isFavoriteSignal.value);
+    server$(() => {
+      console.log("FAVORITE (server)", isFavoriteSignal.value);
+    })();
+  });
+  useStylesScoped$(styles);
+
   return (
     <section class="section bright">
       <p>{dadJokeSignal.value.joke}</p>
+      <Form action={favoriteJokeAction}>
+        <input type="hidden" name="jokeID" value={dadJokeSignal.value.id} />
+        <button name="vote" value="up">
+          👍
+        </button>
+        <button name="vote" value="down">
+          👎
+        </button>
+      </Form>
+      <button
+        onClick$={() => {
+          isFavoriteSignal.value = !isFavoriteSignal.value;
+        }}
+      >
+        {isFavoriteSignal.value ? "❤️" : "🤍"}
+      </button>
     </section>
   );
 });
